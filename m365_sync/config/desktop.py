6bf792019dc7ad7096ae5deb007840b52aed1082
ealecho <elshore32@gmail.com>
2025-11-05 03:11:26 +0300

    Add missing patches.txt and config directory for Frappe app validation

---
+++ m365_sync/config/desktop.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "M365 Sync",
+			"color": "#0078D4",
+			"icon": "octicon octicon-organization",
+			"type": "module",
+			"label": _("M365 Sync")
+		}
+	]
---
+++ m365_sync/config/docs.py
@@ -0,0 +1,4 @@
+source_link = "https://github.com/ealecho/m365_sync"
+docs_base_url = "https://github.com/ealecho/m365_sync"
+headline = "Sync Microsoft 365 groups to ERPNext roles"
+sub_heading = "Automatic role assignment based on M365 group membership"
